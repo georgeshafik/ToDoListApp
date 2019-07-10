d4e99e2ddadba18746bcd2463282b18eca71d6a1
George Shafik <george@gsss.net>
2019-07-10 13:17:13 +1000

    UIColor Extensions + Convenience Initializers. Closed #5

---
+++ GetItDone/extension/extension.swift
@@ -0,0 +1,18 @@
+//
+//  extension.swift
+//  GetItDone
+//
+//  Created by George Shafik on 10/7/19.
+//  Copyright © 2019 George Shafik. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  // convenience allows to write out a new initalizer
+  convenience init(red: Int, green: Int, blue: Int) {
+    self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+  }
+  
+}
