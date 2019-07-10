cd0b4136eadede5b217a23aabdd229dd9de1f18f
George Shafik <george@gsss.net>
2019-07-10 14:44:38 +1000

    Custom Hex Color Code Initializer. Closed #6

--- GetItDone/GDGradient.swift
+++ GetItDone/GDGradient.swift
@@ -15,13 +15,18 @@ class GDGradient:UIView {
     UIColor.init(red: 58, green: 123, blue: 213).cgColor
   ]
   
+  var hexColors:[CGColor] = [
+    UIColor.init(rgb: 0x64E4FF).cgColor,
+    UIColor.init(rgb: 0x3A7BD5).cgColor
+  ]
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     
     self.translatesAutoresizingMaskIntoConstraints = false // you need to set this to false in order to add your own constraints
     
     if let layer = self.layer as? CAGradientLayer {
-      layer.colors = self.colors
+      layer.colors = self.hexColors
       layer.locations = [0.0, 1.2]
     }    
   }
