3a0b017d4681e2aaee6510d6076108188c2c0bfa
George Shafik <george@gsss.net>
2019-07-10 15:13:05 +1000

    Creating and Placing the App Title Label on the Welcome Screen. Closed #8

--- GetItDone/WelcomeController.swift
+++ GetItDone/WelcomeController.swift
@@ -16,18 +16,27 @@ class WelcomeController: UIViewController {
     return view
   }()
   
+  let titleLabel = UILabel()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // root view in our view conntroller is called view
     view.backgroundColor = .white
+    titleLabel.text = "GET IT DONE"
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
     
     view.addSubview(bg)
-    
     bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true // 20 pixels from left
     bg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true // 20 pixels from top
     bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true // 20 pixels from bottom
     bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true // 20 pixels from right
+    
+    view.addSubview(titleLabel)
+    titleLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    titleLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
+    titleLabel.topAnchor.constraint(equalTo: bg.topAnchor, constant: 60).isActive = true
     
   }
 
