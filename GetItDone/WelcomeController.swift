b60758a8dbff1d3343d8b764ec45f30436f1e5f5
George Shafik <george@gsss.net>
2019-07-10 11:37:57 +1000

    Converted Single View App to code only by removing the Storyboard. Closed #1

---
+++ GetItDone/WelcomeController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GetItDone
+//
+//  Created by George Shafik on 10/7/19.
+//  Copyright © 2019 George Shafik. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // root view in our view conntroller is called view
+    view.backgroundColor = .cyan
+    
+  }
+  
+
+
+}
+
