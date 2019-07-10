eb319f3405ea1410bf02035dfd49201c35ee6ffb
George Shafik <george@gsss.net>
2019-07-10 22:36:57 +1000

    How to Use Swift OptionSet(s) to Make our Front-end App Scale with Ease. Closed  #21

--- GetItDone/custom/GDButton.swift
+++ GetItDone/custom/GDButton.swift
@@ -11,32 +11,48 @@ import UIKit
 class GDButton:UIButton {
   
   var title:String!
+  var type:ButtonOptions!
   
   init(title:String = "button text", frame:CGRect = .zero, type:ButtonOptions = .roundedText) {
     super.init(frame: frame)
     if frame == .zero {
       self.translatesAutoresizingMaskIntoConstraints = false
     }
-    
-    switch type {
-      case .roundedText:
-        self.title = title
-        self.phaseTwo()
-      case .squareIcon:
-        break;
-      default: break;
-    }
+
+    self.title = title
+    self.type = type
+    self.phaseTwo()
     
   }
   
   func phaseTwo() {
-    self.setTitle(self.title, for: .normal)
-    self.setTitleColor(.grayZero, for: .normal)
-    self.layer.cornerRadius = 20 // half the height
-    self.backgroundColor = .white
+      self.setTitle(self.title, for: .normal)
+      self.setTitleColor(.grayZero, for: .normal)
+      self.backgroundColor = .white
+
+      if let titleLabel = self.titleLabel {
+        titleLabel.font = UIFont.init(name: "Raleway-Regular", size: 16)
+      }
+    
+      switch self.type {
+        case ButtonOptions.roundedText:
+          self.roundedText()
+        case ButtonOptions.squareIcon:
+          self.squareIcon()
+        default: break;
+      }
+  }
+  
+  func squareIcon() {
+    // thiw is where we will set the customer icons
     if let titleLabel = self.titleLabel {
-      titleLabel.font = UIFont.init(name: "Raleway-Regular", size: 16)
+      titleLabel.font = UIFont.init(name: "Raleway-Regular", size: 24)
     }
+    
+  }
+  
+  func roundedText() {
+    self.layer.cornerRadius = 20 // half the height
   }
   
   required init?(coder aDecoder: NSCoder) {
