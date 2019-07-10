98c351a8a03b3ec24d9eb414e72f2b3448aff940
George Shafik <george@gsss.net>
2019-07-10 12:34:09 +1000

    AutoLayout Constraints Programmatically. Closed #2

--- GetItDone/WelcomeController.swift
+++ GetItDone/WelcomeController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class WelcomeController: UIViewController {
 
+  let bg:UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false // you need to set this to false in order to add your own constraints
+    view.backgroundColor = .cyan
+    view.layer.cornerRadius = 6
+    return view
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // root view in our view conntroller is called view
-    view.backgroundColor = .cyan
+    view.backgroundColor = .white
+    
+    view.addSubview(bg)
+    
+    bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true // 20 pixels from left
+    bg.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true // 20 pixels from top
+    bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true // 20 pixels from bottom
+    bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20).isActive = true // 20 pixels from right
     
   }
   
