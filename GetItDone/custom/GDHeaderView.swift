5589e4535693323718d55cbe9e41b4255f662802
George Shafik <george@gsss.net>
2019-07-10 21:12:16 +1000

    Placing the Add Button Using UIlayoutConstraints. Closed #19

--- GetItDone/custom/GDHeaderView.swift
+++ GetItDone/custom/GDHeaderView.swift
@@ -13,6 +13,7 @@ class GDHeaderView: UIView {
   let bg = GDGradient()
   let titleLabel = GDLabel(size: 14)
   let subtitleLabel = GDLabel(size: 24)
+  let addButton = GDButton(title: "+", type: .squareIcon)
   
   init(frame: CGRect = .zero, title:String = "header title", subtitle:String = "header subtitle") {
     super.init(frame: frame)
@@ -40,6 +41,12 @@ class GDHeaderView: UIView {
     subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
     subtitleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 20 + 8).isActive = true
     subtitleLabel.rightAnchor.constraint(equalTo: centerXAnchor, constant: 50).isActive = true
+    
+    addSubview(addButton)
+    addButton.bottomAnchor.constraint(equalTo: subtitleLabel.bottomAnchor).isActive = true
+    addButton.rightAnchor.constraint(equalTo: rightAnchor, constant: - 20 - 8).isActive = true
+    addButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
+    addButton.widthAnchor.constraint(equalTo: addButton.heightAnchor, multiplier: 1).isActive = true // it will take the height of heightAnchor and multiply it by 1 so the width in this case is 24 * 1 - a square
   }
   
   required init?(coder aDecoder: NSCoder) {
